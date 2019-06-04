6651d512f98f189c34831e9af4442309a7cfca6e
gmocaceres <guillecaceres1989@gmail.com>
2019-06-04 12:03:47 -0400

    ruta y método update funcionando

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -22,6 +22,12 @@ class TodosController < ApplicationController
         @todo = Todo.find(params[:id])
     end
 
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(todo_params)
+        redirect_to todos_path
+    end
+
     private
 
     def todo_params
