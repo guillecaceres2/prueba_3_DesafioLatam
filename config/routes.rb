e4877e9f5c18a5bc708118f599a55491952a84da
gmocaceres <guillecaceres1989@gmail.com>
2019-06-04 12:22:19 -0400

    ruta y método completed funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
   patch 'todos/:id', to: 'todos#update', as: 'update'
   delete 'todos/:id', to: 'todos#destroy', as: 'delete'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'completed'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
